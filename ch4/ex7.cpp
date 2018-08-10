6ceb0303168d47e3f9b029e7a71ce50111b21235
Peter-Chou <zhou_00009@126.com>
2018-08-10 20:46:42 +0800

    add ch5 ex7

---
+++ ch4/ex7.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+struct pizza {
+  string name;
+  double diameter;
+  double weight;
+};
+
+void set_info(pizza& i);
+void display(const pizza& i);
+
+int main() {
+  pizza item1;
+  set_info(item1);
+  display(item1);
+  cin.get();
+  return 0;
+}
+
+void set_info(pizza& i) {
+  cout << "Enter the type of the pizza:\n";
+  getline(cin, i.name);
+  cout << "Enter the diameter of the pizza:\n";
+  cin >> i.diameter;
+  cout << "Enter the weight of the pizza:\n";
+  cin >> i.weight;
+}
+
+void display(const pizza& i) {
+  cout << "the type of the pizza: " << i.name << endl
+       << "diameter: " << i.diameter << endl
+       << "weight: " << i.weight << endl;
+}
