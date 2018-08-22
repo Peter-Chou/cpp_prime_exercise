fc62731be3f3140968294f3216b6fa0648ebfa58
Peter-Chou <zhou_00009@126.com>
2018-08-22 09:06:04 +0800

    remove comments

--- ch8/ex4.cpp
+++ ch8/ex4.cpp
@@ -4,8 +4,8 @@
 using namespace std;
 
 struct stringy {
-  char* str;  // points to a string
-  int ct;     // length of string (not counting '\0')
+  char* str;
+  int ct;
 };
 
 void set(stringy&, char*);
@@ -15,17 +15,13 @@ void show(const char*, int n = 1);
 int main() {
   stringy beany;
   char testing[] = "Reality isn't what it used to be.";
-  set(beany, testing);  // first argument is a reference,
-  // allocates space to hold copy of testing,
-  // sets str member of beany to point to the
-  // new block, copies testing to new block,
-  // and sets ct member of beany
-  show(beany);     // prints member string once
-  show(beany, 2);  // prints member string twice
+  set(beany, testing);
+  show(beany);
+  show(beany, 2);
   testing[0] = 'D';
   testing[1] = 'u';
-  show(testing);     // prints testing string once
-  show(testing, 3);  // prints testing string thrice
+  show(testing);
+  show(testing, 3);
   show("Done!");
   return 0;
 }
